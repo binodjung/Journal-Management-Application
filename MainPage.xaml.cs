dbb42583c52eee1d6e1286bea604762f452852dd
binodjung <bnodtha1122@gmail.com>
2026-01-24 13:58:58 +0545

    Initial  Project Setup

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JournalApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
